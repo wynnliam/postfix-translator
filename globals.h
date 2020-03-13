916823653c1d505ce4a0d773de19641cf023f373
Liam Wynn <wynnliam@pdx.edu>
2020-03-13 11:56:48 -0700

    Moved global variables and values to globals.h

---
+++ globals.h
@@ -0,0 +1,54 @@
+// Liam Wynn, 3/13/2020, Postfix Translator.
+
+/*
+	This file stores all global variables and values.
+	Basically, if more than one file uses it, it's here.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// Possible values for tokenval. We choose
+// values that a char cannot have with the exception
+// of EOS, which in ASCII is 0 and universally means 
+// "end of string".
+#define NONE	-1
+#define EOS		0
+#define NUM 	256
+#define DIV		257
+#define MOD		258
+#define ID		259
+#define DONE	260
+
+// The symbol table holds every id the program sees:
+// "mod", "div", and all id's that start with an alphabet
+// character.
+
+// The symbol table is implemented in two parts:
+// First there is the array of symbols (the symbol_table),
+// and a single character array that holds every id lexeme.
+struct sym_entry {
+	// Points to the first character in a string within
+	// lexemes
+	char* lexptr;
+	// The token value for this entry.
+	int token_val;
+};
+
+struct sym_entry symbol_table[100];
+char lexemes[999];
+
+// The key to predictive parsing. We use this to choose
+// what production rule to follow given the one we are already
+// at. Given how our grammar is structured, we can use this
+// to always correctly choose the next production rule.
+int lookahead;
+// The number value of the current token. For
+// a token we read in, this tells us the type in a way
+// the system can easily understand.
+int tokenval = NONE;
+// Use this for primitive error checking. When a problem
+// occurs, we can at least point out the line that is
+// the offender.
+int line_num = 1;
