07b852c7f8a7e680cfc790c44de3b49e1cbe5e4c
Liam Wynn <wynnliam@pdx.edu>
2020-03-14 08:46:25 -0700

    Added lexer.c. Moved lexan and match to lexer.c

--- main.c
+++ main.c
@@ -87,17 +87,11 @@
 #include "./globals.h"
 
 int line_num = 1;
-int tokenval = NONE;
-
-char lexbuf[LEXBUF_SIZE];
 
 extern void parse();
-// Symbol table functions we use
 extern int lookup(char* lexeme);
 extern int insert(char* lexeme, int token_val);
 
-int lexan();
-void match(const int token);
 void error(const char* message);
 
 int main() {
@@ -108,83 +102,6 @@ int main() {
 
 	return 0;
 }
-
-int lexan() {
-	int t;
-
-	while(1) {
-		t = getchar();
-
-		// Whitespace: just ignore it
-		if(t == ' ' || t == '\t')
-			;
-		else if(t == '\n') {
-			line_num += 1;
-		}
-
-		else if(isdigit(t)) {
-			// Put t back and read the entire number
-			ungetc(t, stdin);
-			scanf("%d", &tokenval);
-			// I'll keep the old version here for your (and my) edification.
-			/*tokenval = t - '0'; // Gives numeric value of t
-			t = getchar();
-			while(isdigit(t)) {
-				// Adding a new digit means increasing token by a factor of 10
-				tokenval = tokenval * 10 + t - '0';
-				t = getchar();
-			}
-
-			// If isdigit(t) == false, we've gone too far, so put t back
-			ungetc(t, stdin);*/
-			return NUM;
-		}
-
-		else if(isalpha(t)) {
-			int sym_table_pos;
-			int buff_pos = 0;
-
-			while(isalnum(t)) {
-				lexbuf[buff_pos] = t;
-				t = getchar();
-				++buff_pos;
-
-				if(buff_pos >= LEXBUF_SIZE)
-					error("compiler error");
-			}
-
-			lexbuf[buff_pos] = EOS;
-			// We are not done with all tokens to read so put
-			// t back.
-			if(t != EOF)
-				ungetc(t, stdin);
-
-			sym_table_pos = lookup(lexbuf);
-
-			if(sym_table_pos == 0)
-				sym_table_pos = insert(lexbuf, ID);
-
-			tokenval = sym_table_pos;
-			return symbol_table[sym_table_pos].token_val;
-		}
-
-		else if(t == EOF)
-			return DONE;
-
-		else {
-			tokenval = NONE;
-			return t;
-		}
-	}
-}
-
-void match(const int token) {
-	if(lookahead == token)
-		lookahead = lexan();
-	else
-		error("Bad token");
-}
-
 
 void error(const char* message) {
 	printf("ERROR: %s\n", message);
