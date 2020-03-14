37a07fab2695bf74d8e2b08f7765de4e88418dd8
Liam Wynn <wynnliam@pdx.edu>
2020-03-13 19:14:40 -0700

    Added some documentation for parsing functions

--- main.c
+++ main.c
@@ -195,6 +195,14 @@ void match(const int token) {
 		error("Bad token");
 }
 
+/*
+	Implements the expr and moreterm production rules:
+
+	expr -> term moreterms
+	moreterms -> + term moreterms
+			  |  - term moreterms
+			  | e
+*/
 void expr() {
 	int t;
 	term();
@@ -211,6 +219,16 @@ void expr() {
 	}
 }
 
+/*
+	Implements the term and morefactors production rules:
+
+	term -> factor morefactors
+	morefactors -> * factor morefactors
+				 | / factor morefactors
+				 | div factor morefactors
+				 | mod factor morefactors
+				 | e
+*/
 void term() {
 	int t;
 	factor();
@@ -233,6 +251,10 @@ void term() {
 	}*/
 }
 
+/*
+	Implements the factor production rule:
+	factor -> ( expr ) | id | num
+*/
 void factor() {
 	if(lookahead == '(') {
 		match('(');
