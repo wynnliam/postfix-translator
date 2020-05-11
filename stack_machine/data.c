db28b4135b8e4389d4320dab28bf12f5ac568a08
Liam Wynn <wynnliam@pdx.edu>
2020-05-11 15:49:42 -0700

    Added all neccessary variables to make data system work.

--- stack_machine/data.c
+++ stack_machine/data.c
@@ -2,3 +2,16 @@
 
 #include "./data.h"
 
+#include <stddef.h>
+
+#define MAX_VARS	1000
+
+struct variable {
+	char* identifier; // The name of the variable used in code.
+	size_t* address; // Pointer into the values array.
+};
+
+static struct variable* variables[MAX_VARS];
+static size_t values[MAX_VARS];
+
+static int num_vars = 0;
