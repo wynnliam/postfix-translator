908c13c72651768e71819165c34725b2a134871b
Liam Wynn <wynnliam@pdx.edu>
2020-05-12 13:07:21 -0700

    Added declaration of rvalue procedure

--- stack_machine/data.h
+++ stack_machine/data.h
@@ -4,5 +4,13 @@
 	Pushes the address of var onto the stack.
 	Side note: if var is not found, then we add
 	it to our data list.
+
+	TODO: Free variables!
 */
 extern void lvalue(const char* var);
+
+/*
+	Pushes the value of a var onto the stack.
+	If the value is not found, we get an error
+*/
+extern void rvalue(const char* var);
