5e7495cc64a8d79133fef9c273a3d459672753de
Liam Wynn <wynnliam@pdx.edu>
2020-05-14 10:47:44 -0700

    Added instruction type values

--- stack_machine/program.h
+++ stack_machine/program.h
@@ -2,6 +2,17 @@
 
 #include <stddef.h>
 
+#define INST_ADD		0
+#define INST_SUBTRACT	INST_ADD + 1
+#define INST_DIVIDE		INST_SUBTRACT + 1
+#define INST_MULT		INST_DIVIDE + 1
+#define INST_PUSH		INST_MULT + 1
+#define INST_POP		INST_PUSH + 1
+#define INST_COPY		INST_POP + 1
+#define INST_RVAL		INST_COPY + 1
+#define INST_LVAL		INST_RVAL + 1
+#define INST_ASSIGN		INST_LVAL + 1
+
 struct instruction {
 	size_t type;
 	union {
