575dd39b9287d55afed94e36134970b3b8c12add
Liam Wynn <wynnliam@pdx.edu>
2020-05-14 12:19:15 -0700

    Added instruction value types for flow control

--- stack_machine/program.h
+++ stack_machine/program.h
@@ -6,12 +6,21 @@
 #define INST_SUBTRACT	INST_ADD + 1
 #define INST_DIVIDE		INST_SUBTRACT + 1
 #define INST_MULT		INST_DIVIDE + 1
+
 #define INST_PUSH		INST_MULT + 1
 #define INST_POP		INST_PUSH + 1
 #define INST_COPY		INST_POP + 1
+
 #define INST_RVAL		INST_COPY + 1
 #define INST_LVAL		INST_RVAL + 1
 #define INST_ASSIGN		INST_LVAL + 1
+
+#define INST_LABEL		INST_ASSIGN + 1
+#define INST_GOTO		INST_LABEL + 1
+#define INST_GOFALSE	INST_GOTO + 1
+#define INST_GOTRUE		INST_GOFALSE + 1
+#define INST_HALT		INST_GOTRUE + 1
+
 
 
 extern void add_instruction(const size_t type, const size_t arg);
