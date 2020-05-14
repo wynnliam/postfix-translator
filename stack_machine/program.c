b447862c1bf50f618b9cfe99135d564884148022
Liam Wynn <wynnliam@pdx.edu>
2020-05-14 10:44:03 -0700

    Added variables needed for executing programs

--- stack_machine/program.c
+++ stack_machine/program.c
@@ -1,3 +1,12 @@
 // Liam Wynn, 5/14/2020, Stack Machine
 
 #include "./program.h"
+
+#define MAX_INSTRUCTIONS	1000
+
+static struct instruction program[MAX_INSTRUCTIONS];
+static int num_instructions = 0;
+
+// Program counter - keeps track of the current instruction we
+// are on. Needed for control flow instructions.
+static int pc = 0;
